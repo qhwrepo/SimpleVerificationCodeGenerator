e44a9897beecc0b6cae843742ada6ffec247f54d
Qinghang Wu <Qinghang Wu>
2016-09-15 10:20:06 +1000

    Completed Program Verification Code Generator

---
+++ VerificationCodeGenerator/VerificationCodeGenerator.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace VerificationCodeGenerator
+{
+    partial class VerificationCodeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Change = new System.Windows.Forms.Label();
+            this.btn_Verify = new System.Windows.Forms.Button();
+            this.txtBox_Code = new System.Windows.Forms.TextBox();
+            this.picBox_Image = new System.Windows.Forms.PictureBox();
+            this.lab_Change = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Change
+            // 
+            this.lbl_Change.AutoSize = true;
+            this.lbl_Change.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_Change.Location = new System.Drawing.Point(209, 64);
+            this.lbl_Change.Name = "lbl_Change";
+            this.lbl_Change.Size = new System.Drawing.Size(41, 12);
+            this.lbl_Change.TabIndex = 0;
+            this.lbl_Change.Text = "Change";
+            this.lbl_Change.Click += new System.EventHandler(this.lbl_Change_Click);
+            // 
+            // btn_Verify
+            // 
+            this.btn_Verify.Location = new System.Drawing.Point(211, 119);
+            this.btn_Verify.Name = "btn_Verify";
+            this.btn_Verify.Size = new System.Drawing.Size(52, 23);
+            this.btn_Verify.TabIndex = 1;
+            this.btn_Verify.Text = "Verify";
+            this.btn_Verify.UseVisualStyleBackColor = true;
+            this.btn_Verify.Click += new System.EventHandler(this.btn_Verify_Click);
+            // 
+            // txtBox_Code
+            // 
+            this.txtBox_Code.Location = new System.Drawing.Point(49, 121);
+            this.txtBox_Code.Name = "txtBox_Code";
+            this.txtBox_Code.Size = new System.Drawing.Size(110, 21);
+            this.txtBox_Code.TabIndex = 2;
+            // 
+            // picBox_Image
+            // 
+            this.picBox_Image.Location = new System.Drawing.Point(49, 42);
+            this.picBox_Image.Name = "picBox_Image";
+            this.picBox_Image.Size = new System.Drawing.Size(110, 34);
+            this.picBox_Image.TabIndex = 3;
+            this.picBox_Image.TabStop = false;
+            // 
+            // lab_Change
+            // 
+            this.lab_Change.AutoSize = true;
+            this.lab_Change.Location = new System.Drawing.Point(211, 28);
+            this.lab_Change.Name = "lab_Change";
+            this.lab_Change.Size = new System.Drawing.Size(41, 12);
+            this.lab_Change.TabIndex = 4;
+            this.lab_Change.TabStop = true;
+            this.lab_Change.Text = "Change";
+            this.lab_Change.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lab_Change_LinkClicked);
+            // 
+            // VerificationCodeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 183);
+            this.Controls.Add(this.lab_Change);
+            this.Controls.Add(this.picBox_Image);
+            this.Controls.Add(this.txtBox_Code);
+            this.Controls.Add(this.btn_Verify);
+            this.Controls.Add(this.lbl_Change);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "VerificationCodeGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VerificationCodeGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Change;
+        private System.Windows.Forms.Button btn_Verify;
+        private System.Windows.Forms.TextBox txtBox_Code;
+        private System.Windows.Forms.PictureBox picBox_Image;
+        private System.Windows.Forms.LinkLabel lab_Change;
+    }
+}
+
---
+++ VerificationCodeGenerator/VerificationCodeGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerificationCodeGenerator
+{
+    public partial class VerificationCodeGenerator : Form
+    {
+        const int Digits = 4;
+        private string Code;
+        public VerificationCodeGenerator()
+        {
+            InitializeComponent();
+            //Generate New V-Code
+            Generate();
+            
+        }
+
+        private void Generate()
+        {
+            Code = GenerateCodeString();
+            Bitmap bm = GenerateImage();
+            picBox_Image.Image = bm;
+        }
+
+        private Bitmap GenerateImage()
+        {
+            Pen pen = new Pen(Color.Black);
+            Font font = new Font("Arial", 14, FontStyle.Bold);
+            Brush brush = null;
+            Bitmap bitmap = new Bitmap(picBox_Image.Width, picBox_Image.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            SizeF totalSizeF = g.MeasureString(Code, font);
+            SizeF curCharSizeF;
+            PointF startPointF = new PointF((picBox_Image.Width - totalSizeF.Width) / 2, (picBox_Image.Height - totalSizeF.Height) / 2);
+            Random random = new Random();
+            g.Clear(Color.White);
+            for (int i = 0; i < Digits; i++)
+            {
+                brush = new LinearGradientBrush(new Point(0, 0), new Point(1, 1), Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)), Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)));
+                g.DrawString(Code[i].ToString(), font, brush, startPointF);
+                curCharSizeF = g.MeasureString(Code[i].ToString(), font);
+                startPointF.X += curCharSizeF.Width;
+            }
+            g.Dispose();
+            return bitmap;
+        }
+
+        private string GenerateCodeString()
+        {
+            char code;
+            string checkcode = string.Empty;
+            Random random = new Random();
+            for (int i = 0; i < Digits; i++)
+            {
+                code = random.Next(0, 1) == 0 ? (char)('0' + (char)random.Next(0, 9)) : (char)('A' + (char)random.Next(0, 25));
+                checkcode += code.ToString();
+            }
+            return checkcode;
+        }
+
+        private void lbl_Change_Click(object sender, EventArgs e)
+        {
+            Generate();
+        }
+
+        private void btn_Verify_Click(object sender, EventArgs e)
+        {
+            if (txtBox_Code.Text == Code)
+                MessageBox.Show("Verification Code Correct");
+            else
+                MessageBox.Show("Verification Code Incorrect");
+            txtBox_Code.Text = "";
+            Generate();
+        }
+
+        private void lab_Change_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Generate();
+        }
+    }
+}
